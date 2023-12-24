2e8aebbc75675ea1695d091bd52be34379b323c6
Ca1len <andro.pershin@gmail.com>
2023-12-24 22:28:06 +0300

    [~] Fix undotree.

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,6 +1,7 @@
 return {
   "mbbill/undotree",
-  keys = {
-    { "n", "<leader>uu", vim.cmd.UndotreeToggle, desc = "Show undotree" },
-  },
+  config = function()
+    vim.keymap.set("n", "<leader>uu", vim.cmd.UndotreeToggle, { desc = "Show undotree" })
+  end,
+  lazy = false,
 }
